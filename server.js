c0b7e7b4d55f9aad293a04db2b9f2e7fada2a759
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-02-02 23:37:50 +0900

    Modify: 서버 포트 수정

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const client = require("cheerio-httpcli");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 const bodyParser = require("body-parser");
 const request = require("request");
 
@@ -25,7 +25,6 @@ app.get("/search", async (req, res) => {
     console.log(userId);
     console.log(problemId);
 
-
     if (req.query.idea1 !== "") {
         ideaList.push(req.query.idea1);
     }
@@ -48,20 +47,19 @@ app.get("/search", async (req, res) => {
 
             request(
                 {
-                url: solvedUrl + queryParams,
+                    url: solvedUrl + queryParams,
                     method: "GET",
                 },
                 function (error, response, body) {
                     const probelmInfo = JSON.parse(body);
                     message += "BOJ " + probelmInfo.problemId + " " + probelmInfo.titleKo + "<br>";
-                    message += "태그 : "
+                    message += "태그 : ";
 
-                    for (let i = 0; i < probelmInfo.tags.length; i++){
+                    for (let i = 0; i < probelmInfo.tags.length; i++) {
                         message += probelmInfo.tags[i].displayNames[0].name;
-                        if (i != probelmInfo.tags.length-1) {
+                        if (i != probelmInfo.tags.length - 1) {
                             message += ", ";
-                        }
-                        else {
+                        } else {
                             message += "<br>";
                         }
                     }
@@ -69,25 +67,25 @@ app.get("/search", async (req, res) => {
                     res();
                 }
             );
-        })
+        });
     }
 
     function careerRefresh() {
         return new Promise((res, rej) => {
-                //사용자 해결 여부 가져오기
-                const userUrl = "https://acmicpc.net/status";
-                queryParams = "?" + encodeURIComponent("problem_id") + "=" + problemId;
-                queryParams += "&" + encodeURIComponent("user_id") + "=" + userId;
-                queryParams += "&" + encodeURIComponent("language_id") + "=" + "-1";
-                queryParams += "&" + encodeURIComponent("result_id") + "=" + "-1";
-                queryParams += "&" + encodeURIComponent("from_problem") + "=" + "1";
+            //사용자 해결 여부 가져오기
+            const userUrl = "https://acmicpc.net/status";
+            queryParams = "?" + encodeURIComponent("problem_id") + "=" + problemId;
+            queryParams += "&" + encodeURIComponent("user_id") + "=" + userId;
+            queryParams += "&" + encodeURIComponent("language_id") + "=" + "-1";
+            queryParams += "&" + encodeURIComponent("result_id") + "=" + "-1";
+            queryParams += "&" + encodeURIComponent("from_problem") + "=" + "1";
 
-                client.set("headers", {
-                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
-                    "Accept-Charset": "utf-8",
-                });
+            client.set("headers", {
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
+                "Accept-Charset": "utf-8",
+            });
 
-                res(userUrl + queryParams);
+            res(userUrl + queryParams);
         });
     }
 
@@ -107,20 +105,20 @@ app.get("/search", async (req, res) => {
             message += "메모리: ```" + memory + "kb```<br>";
             message += "실행 시간: ```" + time + "ms```<br>";
 
-            for (let i = 0; i < ideaList.length; i++){
+            for (let i = 0; i < ideaList.length; i++) {
                 message += "- " + ideaList[i];
                 if (i != ideaList.length - 1) {
                     message += "<br>";
                 }
             }
-        })
+        });
     });
-    
+
     setTimeout(() => {
         return res.status(200).json({
-            form: message
-        })
-    },1000);
+            form: message,
+        });
+    }, 1000);
 });
 
 app.listen(port, () => {
