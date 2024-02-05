b0a528a3e32b664c85983c2588f587dd3c9a9537
Sangwook Park <41150708+sw0501@users.noreply.github.com>
2024-02-05 22:08:24 +0900

    Modify: Function Rename

--- middleware/fetchInfo.js
+++ middleware/fetchInfo.js
@@ -1,79 +1,62 @@
 const request = require("request");
 
-const fetchInfo = async (req, res, next) => {
-
-    const userId = req.query.user;
+const fetchInfoMiddleware = async (req, res, next) => {
+    const userId = req.query.userId;
     const problemId = req.query.problemId;
 
     const checkProblem = () => {
-        new Promise((res, rej) => {
+        return new Promise((res, rej) => {
             const options = {
-                method: 'GET',
-                url: 'https://solved.ac/api/v3/problem/show',
-                qs: {problemId: problemId},
-                headers: {Accept: 'application/json'}
+                method: "GET",
+                url: "https://solved.ac/api/v3/problem/show",
+                qs: { problemId: problemId },
+                headers: { Accept: "application/json" },
             };
 
             request(options, function (error, response, body) {
                 if (error) rej(new Error(error));
-            
-                if (body === "Not Found" || body=== "Bad Request") {
+
+                if (body === "Not Found" || body === "Bad Request") {
                     rej(new Error("Problem Not Found"));
                 }
 
                 res();
-            });    
-        })
-    }
+            });
+        });
+    };
 
     const checkUser = () => {
-        new Promise((res, rej) => {
+        return new Promise((res, rej) => {
             const options = {
-                method: 'GET',
-                url: 'https://solved.ac/api/v3/user/show',
+                method: "GET",
+                url: "https://solved.ac/api/v3/user/show",
                 qs: { handle: userId },
-                headers: {Accept: 'application/json'}
+                headers: { Accept: "application/json" },
             };
-            
+
             request(options, function (error, response, body) {
                 if (error) rej(new Error(error));
-            
-                if (body === "Not Found" || body=== "Bad Request") {
+
+                if (body === "Not Found" || body === "Bad Request") {
                     rej(new Error("User Not Found"));
                 }
 
                 res();
-            }); 
-        })
-    }
+            });
+        });
+    };
 
     try {
+        await checkUser();
+        await checkProblem();
 
-        await checkProblem()
-            .then()
-            .catch(function (error) {
-                return res.status(500).json({
-                    Error: error
-                })
-            });
-
-        await checkUser()
-            .then()
-            .catch(function (error) {
-                return res.status(500).json({
-                    Error: error
-                })
-            });
-        
         next();
-        
     } catch (e) {
-        console.error(`[${req.method}] ${req.path} in :fetchUser middleware - 에러!`, e);
-        return res.status(500).json({
-            error: e,
-            errorString: e.toString(),
+        return res.status(400).json({
+            Error: e,
+            form: e.toString(),
         });
     }
 };
 
-module.exports = fetchInfo;
+module.exports = fetchInfoMiddleware;
